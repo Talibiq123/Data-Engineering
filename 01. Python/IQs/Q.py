df8c9228631cd0fbe4b48412d492342e68d962fc
Talibiq123 <talibsaeed90@gmail.com>
2025-12-01 23:31:41 +0530

    changes

---
+++ 01. Python/IQs/Q.py
@@ -0,0 +1,16 @@
+# Guess the output
+data = {}
+print(type(data))
+
+
+# Guess the output
+print(-10//3)
+
+
+# Print as it is
+# """conversation - He said, "Shantanu's explanation is very good." and I subscribed channel"""
+print('"""conversation - He said, "Shantanu\'s explanation is very good." and I subscribed channel"""')
+
+
+# Guess the output
+print(2**3**2)
